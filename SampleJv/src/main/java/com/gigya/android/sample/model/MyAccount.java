fefa964242ecef99bbe42af10583b2ab2f2e1d56
Tal Mirmelshtein <tal.mirmelshtein@gigya-inc.com>
2019-04-21 13:58:47 +0300

    #87465 - Sample app code updates. Java playground

--- SampleJv/src/main/java/com/gigya/android/sample/model/MyAccount.java
+++ SampleJv/src/main/java/com/gigya/android/sample/model/MyAccount.java
@@ -3,4 +3,29 @@ package com.gigya.android.sample.model;
 import com.gigya.android.sdk.model.account.GigyaAccount;
 
 public class MyAccount extends GigyaAccount {
+
+    private MyData data;
+
+    public MyData getData() {
+        return data;
+    }
+
+    private static class MyData {
+
+        private String comment;
+        private Boolean subscribe;
+        private Boolean terms;
+
+        public String getComment() {
+            return comment;
+        }
+
+        public Boolean getSubscribe() {
+            return subscribe;
+        }
+
+        public Boolean getTerms() {
+            return terms;
+        }
+    }
 }
--- SampleJv/src/main/java/com/gigya/android/sample/ui/MainActivity.java
+++ SampleJv/src/main/java/com/gigya/android/sample/ui/MainActivity.java
@@ -6,21 +6,14 @@ import android.content.Context;
 import android.content.Intent;
 import android.content.IntentFilter;
 import android.os.Bundle;
-import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
-import android.support.design.widget.NavigationView;
 import android.support.v4.content.LocalBroadcastManager;
-import android.support.v4.widget.DrawerLayout;
-import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
-import android.view.Menu;
-import android.view.MenuItem;
 
 import com.gigya.android.sample.R;
 import com.gigya.android.sdk.GigyaDefinitions;
 
-public class MainActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
+public class MainActivity extends AppCompatActivity {
 
     private MainViewModel mViewModel;
 
@@ -29,10 +22,8 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        setTitle("Gigya SDK sample");
+        setTitle("Gigya SDK playground");
         mViewModel = ViewModelProviders.of(this).get(MainViewModel.class);
-
-        initDrawer();
     }
 
     /**
@@ -47,8 +38,10 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
             }
             switch (action) {
                 case GigyaDefinitions.Broadcasts.INTENT_ACTION_SESSION_EXPIRED:
+                    // Session has expired -> SDK session expired event (registration with session expiration).
                     break;
                 case GigyaDefinitions.Broadcasts.INTENT_ACTION_SESSION_INVALID:
+                    // Session is invalid -> SDK session validation interval event.
                     break;
             }
         }
@@ -67,81 +60,5 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
     protected void onPause() {
         LocalBroadcastManager.getInstance(this).unregisterReceiver(mSessionLifecycleReceiver);
         super.onPause();
-    }
-
-    private void initDrawer() {
-        final DrawerLayout drawerLayout = findViewById(R.id.drawer_layout);
-        final Toolbar toolbar = findViewById(R.id.toolbar);
-        final NavigationView navigationView = findViewById(R.id.nav_view);
-        final ActionBarDrawerToggle drawerToggle = new ActionBarDrawerToggle(this, drawerLayout, toolbar,
-                R.string.navigation_drawer_open, R.string.navigation_drawer_close);
-        drawerLayout.addDrawerListener(drawerToggle);
-        drawerToggle.syncState();
-
-        navigationView.setNavigationItemSelectedListener(this);
-        navigationView.getHeaderView(0).setOnClickListener(v -> {
-            if (mViewModel.isLoggedIn()) {
-                // Show account info.
-            }
-        });
-    }
-
-    @Override
-    public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
-        switch (menuItem.getItemId()) {
-            case R.id.api_anonymous:
-                break;
-            case R.id.api_login:
-                break;
-            case R.id.api_login_with_provider:
-                break;
-            case R.id.api_add_connection:
-                break;
-            case R.id.api_remove_connection:
-                break;
-            case R.id.api_register:
-                break;
-            case R.id.api_get_account_info:
-                break;
-            case R.id.api_set_account_info:
-                break;
-            case R.id.api_verify_login:
-                break;
-            case R.id.action_raas:
-                break;
-            case R.id.action_native_login:
-                break;
-        }
-        return false;
-    }
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        getMenuInflater().inflate(R.menu.main, menu);
-
-        final MenuItem accountItem = menu.findItem(R.id.action_account);
-        final MenuItem logoutItem = menu.findItem(R.id.action_logout);
-
-        accountItem.setVisible(mViewModel.isLoggedIn());
-        logoutItem.setVisible(mViewModel.isLoggedIn());
-        
-       return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        switch (item.getItemId()) {
-            case R.id.action_account:
-                break;
-            case R.id.action_clear:
-                break;
-            case R.id.action_reinit:
-                break;
-            case R.id.action_logout:
-                break;
-            case R.id.toggle_interruptions:
-                break;
-        }
-        return super.onOptionsItemSelected(item);
     }
 }
