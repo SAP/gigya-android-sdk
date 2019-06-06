dbca13c29c2db2b614993ef7f855dd20cabba3a8
Tal Mirmelshtein <tal.mirmelshtein@gigya-inc.com>
2019-06-06 09:47:56 +0300

    #87465 - Added blueprint test files for interruption tests.

---
+++ GigyaSDK/src/test/java/com/gigya/android/interruption/LinkAccountInterruptionTest.java
@@ -0,0 +1,23 @@
+package com.gigya.android.interruption;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.powermock.modules.junit4.PowerMockRunner;
+
+@RunWith(PowerMockRunner.class)
+public class LinkAccountInterruptionTest {
+
+
+    @Before
+    public void setup() {
+
+    }
+
+    @Test
+    public void testLinkToSocial() {
+
+    }
+
+
+}
