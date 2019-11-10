0e05adc9ec5fc56d95640fb5d059521c05b3a419
Tal Mirmelshtein <tal.mirmelshtein@gigya-inc.com>
2019-11-10 10:55:36 +0200

    #104079 - Updated SDK release versions to 4.0.6

--- GigyaSDK/src/main/java/com/gigya/android/sdk/Gigya.java
+++ GigyaSDK/src/main/java/com/gigya/android/sdk/Gigya.java
@@ -39,7 +39,7 @@ import java.util.TreeMap;
 public class Gigya<T extends GigyaAccount> {
 
     //region static
-    public static final String VERSION = "4.0.5";
+    public static final String VERSION = "4.0.6";
 
     private static final String LOG_TAG = "Gigya";
 
