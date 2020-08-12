6a4b55078043b3b86939d78cd07f065f72e324ab
Tal Mirmelshtein <tal.mirmelshtein@sap.com>
2020-08-12 11:34:20 +0300

    #117151 - NSS image support minor updates.

--- sdk-nss/src/main/java/com/gigya/android/sdk/nss/bloc/data/NssDataResolver.kt
+++ sdk-nss/src/main/java/com/gigya/android/sdk/nss/bloc/data/NssDataResolver.kt
@@ -7,10 +7,14 @@ import io.flutter.plugin.common.MethodChannel
 import java.io.ByteArrayOutputStream
 
 
-class DataResolver(
+class NssDataResolver(
         private val context: Context) {
 
-    private fun fetchResource(res: String): ByteArray {
+    /**
+     * Generate a bitmap from a given resource Id and decode it.
+     * Decoded byte array will be passed the the NSS engine and reconstructed.
+     */
+    private fun getBitmapDataFromResourceId(res: String): ByteArray {
         val id = context.resources.getIdentifier(res, "drawable", context.packageName)
         val bitmap: Bitmap = BitmapFactory.decodeResource(context.resources, id)
         val stream = ByteArrayOutputStream()
@@ -21,15 +25,20 @@ class DataResolver(
         return byteArray
     }
 
+    /**
+     * Handle specific incoming data requests from the NSS engine.
+     * Specific UI components are able to send data requests in order to receive specific native data.
+     */
     fun handleDataRequest(request: String, arguments: MutableMap<String, Any>, result: MethodChannel.Result) {
         when (request) {
             "image_resource" -> {
                 val url: String? = arguments["url"] as String?
                 if (url == null) {
-                    result.error("400", "", "")
+                    // Return empty data. Should direct to fallback.
+                    result.success(null)
                     return
                 }
-                val byteArray: ByteArray = fetchResource(url)
+                val byteArray: ByteArray = getBitmapDataFromResourceId(url)
                 result.success(byteArray)
             }
         }
