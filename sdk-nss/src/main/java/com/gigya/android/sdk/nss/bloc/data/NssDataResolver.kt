e0bd26b8452786d7d2f9c5b032c0b11fd4f98513
Tal Mirmelshtein <tal.mirmelshtein@sap.com>
2020-08-23 12:09:01 +0300

    #118857 - NSS added support for profile photo component.

--- sdk-nss/src/main/java/com/gigya/android/sdk/nss/bloc/data/NssDataResolver.kt
+++ sdk-nss/src/main/java/com/gigya/android/sdk/nss/bloc/data/NssDataResolver.kt
@@ -1,24 +1,46 @@
 package com.gigya.android.sdk.nss.bloc.data
 
+import android.annotation.SuppressLint
 import android.content.Context
+import android.content.Intent
 import android.graphics.Bitmap
 import android.graphics.BitmapFactory
+import android.net.Uri
+import android.os.Build
+import android.provider.MediaStore
+import android.support.annotation.RequiresApi
+import com.gigya.android.sdk.GigyaLogger
 import io.flutter.plugin.common.MethodChannel
 import java.io.ByteArrayOutputStream
 
 
-class NssDataResolver(
-        private val context: Context) {
+class NssDataResolver(private val context: Context) {
+
+    companion object {
+        const val LOG_TAG = "NssDataResolver"
+    }
 
     /**
      * Generate a bitmap from a given resource Id and decode it.
      * Decoded byte array will be passed the the NSS engine and reconstructed.
      */
-    private fun getBitmapDataFromResourceId(res: String): ByteArray {
+    private fun getBitmapDataFromResourceId(res: String): ByteArray? {
         val id = context.resources.getIdentifier(res, "drawable", context.packageName)
         val bitmap: Bitmap = BitmapFactory.decodeResource(context.resources, id)
+        return getBitmapData(bitmap)
+    }
+
+    fun getBitmapDataFromUri(uri: Uri): ByteArray? {
+        val bitmap: Bitmap? = MediaStore.Images.Media.getBitmap(context.contentResolver, uri)
+        bitmap?.let {
+            return getBitmapData(it)
+        }
+        return null
+    }
+
+    fun getBitmapData(bitmap: Bitmap): ByteArray? {
         val stream = ByteArrayOutputStream()
-        bitmap.compress(Bitmap.CompressFormat.PNG, 100, stream)
+        bitmap.compress(Bitmap.CompressFormat.JPEG, 100, stream)
         val byteArray: ByteArray = stream.toByteArray()
         bitmap.recycle()
         stream.close()
@@ -26,21 +48,31 @@ class NssDataResolver(
     }
 
     /**
-     * Handle specific incoming data requests from the NSS engine.
-     * Specific UI components are able to send data requests in order to receive specific native data.
+     * Fetch image data from resource according to provided Id.
+     * Byte data will be injected back to the engine for display.
      */
-    fun handleDataRequest(request: String, arguments: MutableMap<String, Any>, result: MethodChannel.Result) {
-        when (request) {
-            "image_resource" -> {
-                val url: String? = arguments["url"] as String?
-                if (url == null) {
-                    // Return empty data. Should direct to fallback.
-                    result.success(null)
-                    return
-                }
-                val byteArray: ByteArray = getBitmapDataFromResourceId(url)
-                result.success(byteArray)
-            }
+    fun fetchImageResource(arguments: MutableMap<String, Any>, result: MethodChannel.Result) {
+        val url: String? = arguments["url"] as String?
+        if (url == null) {
+            // Return empty data. Should direct to fallback.
+            result.success(null)
+            return
         }
+        val byteArray: ByteArray? = getBitmapDataFromResourceId(url)
+        result.success(byteArray)
+    }
+
+    /**
+     * Compose an image selection intent (capture & gallery options).
+     */
+    @SuppressLint("InlinedApi")
+    fun imageSelectionIntent(): Intent {
+        val capture = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
+        val select = Intent(Intent.ACTION_GET_CONTENT).addCategory(Intent.CATEGORY_OPENABLE)
+        select.type = "image/*"
+        val chooserIntent = Intent(Intent.ACTION_CHOOSER)
+        chooserIntent.putExtra(Intent.EXTRA_INTENT, select)
+        chooserIntent.putExtra(Intent.EXTRA_INITIAL_INTENTS, arrayOf(capture))
+        return chooserIntent
     }
 }
