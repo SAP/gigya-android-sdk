79c6bab4c0754650def9394c42eedb14d86a7031
Tal Mirmelshtein <tal.mirmelshtein@sap.com>
2020-08-23 12:09:01 +0300

    #118857 - NSS added support for profile photo component.
    Former-commit-id: 41398edc4403e7d7997915412a25b4d9c9294c30

--- sdk-nss/src/main/java/com/gigya/android/sdk/nss/bloc/action/NssSetAccountAction.kt
+++ sdk-nss/src/main/java/com/gigya/android/sdk/nss/bloc/action/NssSetAccountAction.kt
@@ -1,11 +1,15 @@
 package com.gigya.android.sdk.nss.bloc.action
 
+import android.annotation.SuppressLint
+import android.util.Base64
 import com.gigya.android.sdk.GigyaCallback
 import com.gigya.android.sdk.GigyaLogger
 import com.gigya.android.sdk.account.models.GigyaAccount
+import com.gigya.android.sdk.api.GigyaApiResponse
 import com.gigya.android.sdk.api.IBusinessApiService
 import com.gigya.android.sdk.interruption.IPendingRegistrationResolver
 import com.gigya.android.sdk.network.GigyaError
+import com.gigya.android.sdk.network.adapter.RestAdapter
 import com.gigya.android.sdk.nss.bloc.flow.INssFlowDelegate
 import com.gigya.android.sdk.nss.bloc.flow.NssResolver
 import com.gigya.android.sdk.nss.utils.guard
@@ -24,7 +28,17 @@ class NssSetAccountAction<T : GigyaAccount>(private val businessApi: IBusinessAp
                 " honors, publications,  patents, certifications, professionalHeadline, bio, industry," +
                 " specialties, work, skills, religion, politicalView, interestedIn, relationshipStatus," +
                 " hometown, favorites, followersCount, followingCount, username, name, locale, verified, timezone, likes, samlData"
+
+        const val setProfilePhoto = "setProfilePhoto"
+
+        const val SET_PROFILE_ENGINE_GENERAL_ERROR = "500"
+        const val SET_PROFILE_ENGINE_SIZE_ERROR = "413004"
     }
+
+    /**
+     * Set temporary profile photo flag.
+     */
+    var publishPhotoOnSubmit = false
 
     /**
      * Action may use a resolver as part of a registration/login flow.
@@ -67,22 +81,103 @@ class NssSetAccountAction<T : GigyaAccount>(private val businessApi: IBusinessAp
             GigyaLogger.error(LOG_TAG, "Action flow delegate not set")
         }
 
+        // Run super first to allow general action handling.
         super.onNext(method, arguments)
-        if (method == submit) {
 
-            flattenArguments(arguments)
-
-            if (pendingRegistrationResolver != null) {
-                // Resolve interruption as part of a registration/login flow.
-                pendingRegistrationResolver?.setAccount(arguments!!)
-
-            } else {
-                flowDelegate!!.refined<INssFlowDelegate<T>> {
-                    // Apply default set account action using the business api.
-                    businessApi.setAccount(arguments, it.getMainFlowCallback())
+        when (method) {
+            submit -> {
+                setAccount(arguments)
+            }
+            setProfilePhoto -> {
+                arguments?.let {
+                    setProfilePhoto(it["data"] as ByteArray)
                 }
             }
         }
     }
 
+    var profileImageResult: MethodChannel.Result? = null
+
+    /**
+     * Submit account form. Update the account data.
+     */
+    private fun setAccount(arguments: MutableMap<String, Any>?) {
+        flattenArguments(arguments)
+        if (pendingRegistrationResolver != null) {
+            // Resolve interruption as part of a registration/login flow.
+            pendingRegistrationResolver?.setAccount(arguments!!)
+        } else {
+            flowDelegate!!.refined<INssFlowDelegate<T>> {
+
+                // Apply default set account action using the business api.
+                businessApi.setAccount(arguments, it.getMainFlowCallback())
+
+                // If a temporary profile photo is available, publish it.
+                if (publishPhotoOnSubmit) {
+                    publishProfilePhoto()
+                }
+            }
+        }
+    }
+
+
+    /**
+     * Set new profile photo.
+     * Endpoint is used without the "publish" tag.
+     * In order to publish the screen form will need to be submitted.
+     */
+    @SuppressLint("NewApi")
+    fun setProfilePhoto(data: ByteArray) {
+        // Need to check the size of the image. We cant allow file upload that exceeds 6 MB.
+        val imageSize = data.size / (1024 * 1025)
+        if (imageSize >= 6) {
+            profileImageResult?.error(SET_PROFILE_ENGINE_SIZE_ERROR,
+                    "Image size exceeds 6 MB", null)
+            return
+        }
+        // Encode & send image.
+        val encoded = Base64.encodeToString(data, Base64.NO_PADDING and Base64.NO_WRAP)
+        val params = mapOf<String, Any>("photoBytes" to encoded)
+        businessApi.send(
+                "accounts.setProfilePhoto",
+                params,
+                RestAdapter.HttpMethod.POST.intValue(),
+                GigyaApiResponse::class.java, object : GigyaCallback<GigyaApiResponse>() {
+            override fun onSuccess(obj: GigyaApiResponse?) {
+                GigyaLogger.debug(LOG_TAG, "setProfilePhoto: success")
+                publishPhotoOnSubmit = true
+                profileImageResult?.success(data)
+            }
+
+            override fun onError(error: GigyaError?) {
+                GigyaLogger.debug(LOG_TAG, "setProfilePhoto: failed")
+                publishPhotoOnSubmit = false
+                profileImageResult?.error(SET_PROFILE_ENGINE_GENERAL_ERROR,
+                        "Failed to update profile photo", null)
+            }
+        })
+    }
+
+
+    /**
+     * Publish requested profile photo along side with the submission form.
+     * Currently no result/error handling is relevant.
+     */
+    private fun publishProfilePhoto() {
+        businessApi.send(
+                "accounts.publishProfilePhoto",
+                null,
+                RestAdapter.HttpMethod.POST.intValue(),
+                GigyaApiResponse::class.java, object : GigyaCallback<GigyaApiResponse>() {
+            override fun onSuccess(obj: GigyaApiResponse?) {
+                GigyaLogger.debug(LOG_TAG, "publishProfilePhoto: success")
+                publishPhotoOnSubmit = false
+            }
+
+            override fun onError(error: GigyaError?) {
+                GigyaLogger.debug(LOG_TAG, "publishProfilePhoto: failed")
+                publishPhotoOnSubmit = false
+            }
+        })
+    }
 }
