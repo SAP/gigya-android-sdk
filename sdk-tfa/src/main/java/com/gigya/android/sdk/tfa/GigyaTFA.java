33fa61a0e2282fd8feadcb4f8fa582736236d69c
Tal Mirmelshtein <tal.mirmelshtein@sap.com>
2020-10-13 12:35:13 +0300

    #122398 - Versioning update.

--- sdk-tfa/src/main/java/com/gigya/android/sdk/tfa/GigyaTFA.java
+++ sdk-tfa/src/main/java/com/gigya/android/sdk/tfa/GigyaTFA.java
@@ -35,10 +35,9 @@ import com.gigya.android.sdk.tfa.ui.PushTFAActivity;
 
 import static com.gigya.android.sdk.tfa.GigyaDefinitions.TFA_CHANNEL_ID;
 
-@SuppressWarnings("Convert2Lambda")
 public class GigyaTFA {
 
-    private static final String VERSION = "1.0.4";
+    private static final String VERSION = "2.0.0";
 
     private static final String LOG_TAG = "GigyaTFA";
 
