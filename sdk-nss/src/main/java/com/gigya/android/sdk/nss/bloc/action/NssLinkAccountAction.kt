139a87ce522ae540a20d3894dbbf4236416935cd
Tal Mirmelshtein <tal.mirmelshtein@sap.com>
2020-10-28 13:12:04 +0200

    #119347 - NSS link account action added.
    Former-commit-id: b94050fa4dbc01bdcbc0de82de9362a23e7a0aac

---
+++ sdk-nss/src/main/java/com/gigya/android/sdk/nss/bloc/action/NssLinkAccountAction.kt
@@ -0,0 +1,64 @@
+package com.gigya.android.sdk.nss.bloc.action
+
+import com.gigya.android.sdk.GigyaLogger
+import com.gigya.android.sdk.account.models.GigyaAccount
+import com.gigya.android.sdk.api.IBusinessApiService
+import com.gigya.android.sdk.interruption.link.LinkAccountsResolver
+import com.gigya.android.sdk.nss.bloc.flow.NssResolver
+import com.gigya.android.sdk.nss.utils.guard
+import com.gigya.android.sdk.nss.utils.refined
+import com.gigya.android.sdk.nss.utils.serializeToMap
+import io.flutter.plugin.common.MethodChannel
+
+class NssLinkAccountAction<T : GigyaAccount>(private val businessApi: IBusinessApiService<T>) : NssAction<T>(businessApi) {
+
+    companion object {
+        const val LOG_TAG = "NssLinkAccountAction"
+    }
+
+    override fun initialize(result: MethodChannel.Result) {
+        // Inject conflicting account data back to engine.
+        flowDelegate?.getResolver()?.let { nssResolver ->
+            nssResolver.refined<NssResolver<LinkAccountsResolver<T>>> { linkAccountResolver ->
+                val data = linkAccountResolver.resolver.conflictingAccounts.serializeToMap(flowDelegate!!.getGson())
+                result.success(data)
+            }
+        }
+    }
+
+    override fun onNext(method: String, arguments: MutableMap<String, Any>?) {
+        flowDelegate?.guard {
+            GigyaLogger.error(NssSetAccountAction.LOG_TAG, "Action flow delegate not set")
+        }
+
+        flowDelegate!!.getResolver()?.refined<NssResolver<LinkAccountsResolver<T>>> { resolver ->
+            when (method) {
+                socialLogin -> {
+                    // Link to social.
+                    val provider = arguments?.get("provider") as? String
+                    provider?.guard {
+                        GigyaLogger.error(NssAction.LOG_TAG, "Social provider unavailable")
+                    }
+
+                    resolver.resolver.linkToSocial(provider)
+                }
+                submit -> {
+                    // Link to site.
+                    val loginID = arguments?.get("loginID") as? String
+                    loginID?.guard {
+                        GigyaLogger.error(NssAction.LOG_TAG, "Missing required loginID parameter")
+                    }
+
+                    val pass = arguments?.get("password") as? String
+                    pass?.guard {
+                        GigyaLogger.error(NssAction.LOG_TAG, "Missing required password parameter")
+                    }
+
+                    resolver.resolver.linkToSite(loginID, pass)
+                }
+            }
+        }
+
+    }
+
+}
