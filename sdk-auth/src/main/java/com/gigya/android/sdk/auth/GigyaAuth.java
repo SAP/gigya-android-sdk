9befc8a9f96d15594cce83c57873efeb125f8f2d
Tal Mirmelshtein <tal.mirmelshtein@sap.com>
2021-03-15 11:03:03 +0200

    #00000 - Version code updates (core, auth)
    Former-commit-id: 0728e7156c267a7294a1864966291fe5cd04d0e6

--- sdk-auth/src/main/java/com/gigya/android/sdk/auth/GigyaAuth.java
+++ sdk-auth/src/main/java/com/gigya/android/sdk/auth/GigyaAuth.java
@@ -43,7 +43,7 @@ import static com.gigya.android.sdk.auth.GigyaDefinitions.AUTH_CHANNEL_ID;
 
 public class GigyaAuth {
 
-    private static final String VERSION = "2.0.0";
+    private static final String VERSION = "2.1.0";
 
     private static final String LOG_TAG = "GigyaAuth";
 
