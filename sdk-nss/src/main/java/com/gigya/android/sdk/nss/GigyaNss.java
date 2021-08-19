e16a27ae657f13cb7e1c6489269e3bb68bc53a3f
Tal Mirmelshtein <tal.mirmelshtein@sap.com>
2021-08-19 12:12:40 +0300

    Nss update 1.3.1 support.

--- sdk-nss/src/main/java/com/gigya/android/sdk/nss/GigyaNss.java
+++ sdk-nss/src/main/java/com/gigya/android/sdk/nss/GigyaNss.java
@@ -28,7 +28,7 @@ import com.gigya.android.sdk.nss.engine.NssEngineLifeCycle;
 
 public class GigyaNss {
 
-    public static final String VERSION = "1.2.0";
+    public static final String VERSION = "1.3.1";
 
     private static final String LOG_TAG = "GigyaNss";
 
