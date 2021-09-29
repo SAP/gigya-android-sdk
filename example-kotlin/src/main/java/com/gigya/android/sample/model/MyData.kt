10c1d7cf1f4947be12cab3b461fbb5e422d1bc60
Tal Mirmelshtein <tal.mirmelshtein@sap.com>
2021-09-29 19:33:56 +0300

    Updates for NSS engine v1.4.0

--- example-kotlin/src/main/java/com/gigya/android/sample/model/MyData.kt
+++ example-kotlin/src/main/java/com/gigya/android/sample/model/MyData.kt
@@ -1,11 +1,14 @@
 package com.gigya.android.sample.model
 
 
-data class MyData(var subscribe: Boolean?,
-                  var terms: Boolean?,
-                  var comment: String?,
-                  var favGuitar: String?,
-                  var rescueString: String?,
-                  var mark: Int = 0,
-                  var marketingNotifications: Boolean? = false,
-                  var formatTrue: Boolean? = false)
+data class MyData(
+    var subscribe: Boolean?,
+    var terms: Boolean?,
+    var comment: String?,
+    var favGuitar: String?,
+    var rescueString: String?,
+    var mark: Int = 0,
+    var marketingNotifications: Boolean? = false,
+    var formatTrue: Boolean? = false,
+    var dateString: String?
+)
