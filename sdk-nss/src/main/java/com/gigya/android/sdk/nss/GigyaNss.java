5615c76fad932cf27cde1402bcfc4eb9be3e002a
Tal Mirmelshtein <tal.mirmelshtein@sap.com>
2021-11-09 13:08:28 +0200

    NSS release v1.4.2 + engine build.
    Former-commit-id: b20e00e88546dcb64d94ecfd0f1a296007ba2143

--- sdk-nss/src/main/java/com/gigya/android/sdk/nss/GigyaNss.java
+++ sdk-nss/src/main/java/com/gigya/android/sdk/nss/GigyaNss.java
@@ -28,7 +28,7 @@ import com.gigya.android.sdk.nss.engine.NssEngineLifeCycle;
 
 public class GigyaNss {
 
-    public static final String VERSION = "1.4.1";
+    public static final String VERSION = "1.4.2";
 
     private static final String LOG_TAG = "GigyaNss";
 
