fe02d3a8408b9859f40c4c56179952873c5159aa
Tal Mirmelshtein <tal.mirmelshtein@sap.com>
2022-07-14 01:57:01 +0300

    Added custom header support for gigya request.
    Former-commit-id: 609d981f717f0a53484dbfa71157b07e37d6181e

--- sdk-core/src/main/java/com/gigya/android/sdk/api/GigyaApiRequest.java
+++ sdk-core/src/main/java/com/gigya/android/sdk/api/GigyaApiRequest.java
@@ -1,9 +1,11 @@
 package com.gigya.android.sdk.api;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 
 import com.gigya.android.sdk.network.adapter.RestAdapter;
 
+import java.util.HashMap;
 import java.util.TreeMap;
 
 public class GigyaApiRequest {
@@ -13,43 +15,62 @@ public class GigyaApiRequest {
     private RestAdapter.HttpMethod method;
     @NonNull
     private TreeMap<String, Object> params;
+    @Nullable
+    private HashMap<String, String> headers;
 
     /*
     Anonymous requests will not be signed using timestamp, nonce & signature.
      */
     private boolean isAnonymous = false;
 
-    public GigyaApiRequest(RestAdapter.HttpMethod method, @NonNull String api, @NonNull TreeMap<String, Object> params) {
+    public GigyaApiRequest(RestAdapter.HttpMethod method,
+                           @NonNull String api,
+                           @NonNull TreeMap<String, Object> params) {
         this.method = method;
         this.api = api;
         this.params = params;
     }
 
+    public GigyaApiRequest(RestAdapter.HttpMethod method,
+                           @NonNull String api,
+                           @NonNull TreeMap<String, Object> params,
+                           @Nullable HashMap<String, String> headers) {
+        this.method = method;
+        this.api = api;
+        this.params = params;
+        this.headers = headers;
+    }
+
     public RestAdapter.HttpMethod getMethod() {
-        return method;
+        return this.method;
     }
 
     public boolean isAnonymous() {
-        return isAnonymous;
+        return this.isAnonymous;
     }
 
     public void setAnonymous(boolean anonymous) {
-        isAnonymous = anonymous;
+        this.isAnonymous = anonymous;
     }
 
     @NonNull
     public String getTag() {
-        return api;
+        return this.api;
     }
 
     @NonNull
     public String getApi() {
-        return api;
+        return this.api;
     }
 
     @NonNull
     public TreeMap<String, Object> getParams() {
-        return params;
+        return this.params;
+    }
+
+    @Nullable
+    public HashMap<String, String> getHeaders() {
+        return this.headers;
     }
 
 }
