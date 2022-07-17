27f829f4c25184f25df118935071b3a49aa94ff3
Tal Mirmelshtein <tal.mirmelshtein@sap.com>
2022-07-17 20:30:44 +0300

    Basic error & callback flows.

--- sdk-core/src/main/java/com/gigya/android/sdk/auth/IWebAuthnService.java
+++ sdk-core/src/main/java/com/gigya/android/sdk/auth/IWebAuthnService.java
@@ -1,17 +1,19 @@
 package com.gigya.android.sdk.auth;
 
 
-import android.content.Intent;
-
 import androidx.activity.result.ActivityResult;
 import androidx.activity.result.ActivityResultLauncher;
 import androidx.activity.result.IntentSenderRequest;
 
+import com.gigya.android.sdk.GigyaCallback;
+import com.gigya.android.sdk.GigyaLoginCallback;
+import com.gigya.android.sdk.api.GigyaApiResponse;
+
 public interface IWebAuthnService {
 
-    void register(ActivityResultLauncher<IntentSenderRequest> resultLauncher);
+    void register(ActivityResultLauncher<IntentSenderRequest> resultLauncher, GigyaCallback<GigyaApiResponse> gigyaCallback);
 
-    void login(ActivityResultLauncher<IntentSenderRequest> resultLauncher);
+    void login(ActivityResultLauncher<IntentSenderRequest> resultLauncher, GigyaCallback<GigyaApiResponse> gigyaCallback);
 
     void handleFidoResult(ActivityResult activityResult);
 
