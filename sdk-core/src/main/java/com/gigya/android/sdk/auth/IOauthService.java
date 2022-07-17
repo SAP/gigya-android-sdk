c9b61abf74f335030613b03ace7c2a205896597c
Tal Mirmelshtein <tal.mirmelshtein@sap.com>
2022-07-17 20:30:44 +0300

    Basic error & callback flows.
    Former-commit-id: c0609d1d43ca30724e7358a6e6a441e108ac8afb

--- sdk-core/src/main/java/com/gigya/android/sdk/auth/IOauthService.java
+++ sdk-core/src/main/java/com/gigya/android/sdk/auth/IOauthService.java
@@ -1,10 +1,11 @@
 package com.gigya.android.sdk.auth;
 
+import com.gigya.android.sdk.api.ApiService;
+
 public interface IOauthService {
 
-    void connect(String token);
+    void connect(String token, ApiService.IApiServiceResponse iApiServiceResponse);
+    void authorize(String token, ApiService.IApiServiceResponse iApiServiceResponse);
 
-    void authorize(String token);
-
-    void token(String code);
+    void token(String code, ApiService.IApiServiceResponse iApiServiceResponse);
 }
