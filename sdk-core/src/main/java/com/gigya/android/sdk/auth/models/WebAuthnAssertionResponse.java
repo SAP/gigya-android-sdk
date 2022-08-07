9c52e84d8e2264f05cae45d0f1bcdf5a9449e9e7
Tal Mirmelshtein <tal.mirmelshtein@sap.com>
2022-08-07 22:45:20 +0300

    Removed use of origin manipulation.

--- sdk-core/src/main/java/com/gigya/android/sdk/auth/models/WebAuthnAssertionResponse.java
+++ sdk-core/src/main/java/com/gigya/android/sdk/auth/models/WebAuthnAssertionResponse.java
@@ -1,26 +1,28 @@
 package com.gigya.android.sdk.auth.models;
 
 import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
 
 import java.util.HashMap;
 import java.util.Map;
 
 public class WebAuthnAssertionResponse {
 
+    public Object userHandleBase64;
     public String clientDataJSONBase64;
     public String authenticatorDataBase64;
     public String signatureBase64;
     public String idBase64;
     public String rawIdBase64;
 
-    private final Gson gson = new Gson();
-
     public WebAuthnAssertionResponse(
+            Object userHandleBase64,
             String clientDataJSONBase64,
             String authenticatorDataBase64,
             String signatureBase64,
             String idBase64,
             String rawIdBase64) {
+        this.userHandleBase64 = userHandleBase64;
         this.clientDataJSONBase64 = clientDataJSONBase64;
         this.authenticatorDataBase64 = authenticatorDataBase64;
         this.signatureBase64 = signatureBase64;
@@ -28,12 +30,12 @@ public class WebAuthnAssertionResponse {
         this.rawIdBase64 = rawIdBase64;
     }
 
-    private Map<String, String> getResponse() {
-        Map<String, String> response = new HashMap<>();
+    private Map<String, Object> getResponse() {
+        Map<String, Object> response = new HashMap<>();
         response.put("authenticatorData", this.authenticatorDataBase64);
         response.put("clientDataJSON", this.clientDataJSONBase64);
         response.put("signature", signatureBase64);
-        response.put("userHandle", rawIdBase64); //TODO user keyhandle or rawid like google requests.
+        response.put("userHandle", userHandleBase64); //TODO user keyhandle or rawid like google requests.
         return response;
     }
 
@@ -43,7 +45,10 @@ public class WebAuthnAssertionResponse {
         assertion.put("rawId", rawIdBase64);
         assertion.put("type", "public-key");
         assertion.put("response", getResponse());
-        return gson.toJson(assertion);
+
+        final GsonBuilder builder = new GsonBuilder();
+        builder.serializeNulls();
+        return builder.create().toJson(assertion);
     }
 
 }
