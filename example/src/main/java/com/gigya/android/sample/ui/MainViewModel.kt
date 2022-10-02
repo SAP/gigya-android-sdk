dd0873b8983096fef5d123130b2bbd9f018f6a2b
Tal Mirmelshtein <tal.mirmelshtein@sap.com>
2022-10-02 10:56:51 +0300

    Updated gradle plugin (7.3) + example apps.

---
+++ example/src/main/java/com/gigya/android/sample/ui/MainViewModel.kt
@@ -0,0 +1,123 @@
+package com.gigya.android.sample.ui
+
+import androidx.activity.result.ActivityResultLauncher
+import androidx.activity.result.IntentSenderRequest
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.gigya.android.sample.model.MyAccount
+import com.gigya.android.sample.repository.GigyaRepository
+import com.gigya.android.sdk.GigyaPluginCallback
+import com.gigya.android.sdk.network.GigyaError
+import com.gigya.android.sdk.ui.plugin.GigyaPluginEvent
+import kotlinx.coroutines.launch
+
+class MainViewModel : ViewModel() {
+
+    private val gigyaRepository = GigyaRepository()
+
+    val account: MutableLiveData<MyAccount> by lazy {
+        MutableLiveData<MyAccount>()
+    }
+
+    fun isLoggedIn() = gigyaRepository.isLoggedIn()
+
+    // Login using email & password pair.
+    fun credentialLogin(email: String, password: String,
+                        error: (GigyaError?) -> Unit, onLogin: () -> Unit) {
+        val params = mutableMapOf<String, Any>("loginID" to email, "password" to password)
+        viewModelScope.launch {
+            val result = gigyaRepository.login(params)
+            if (result.isError()) {
+                error(result.error)
+                return@launch
+            }
+            account.value = result.account
+            onLogin()
+        }
+    }
+
+    fun credentialRegister(email: String, password: String,
+                           error: (GigyaError?) -> Unit, onLogin: () -> Unit) {
+        viewModelScope.launch {
+            val result = gigyaRepository.register(email, password)
+            if (result.isError()) {
+                error(result.error)
+                return@launch
+            }
+            account.value = result.account
+            onLogin()
+        }
+    }
+
+    // Request new account data ("getAccount" endpoint).
+    fun getAccount(error: (GigyaError?) -> Unit) {
+        viewModelScope.launch {
+            val result = gigyaRepository.getAccountInfo()
+            if (result.isError()) {
+                error(result.error)
+                return@launch
+            }
+            account.value = result.account
+        }
+    }
+
+    fun logout(error: (GigyaError?) -> Unit, onLogout: () -> Unit) {
+        viewModelScope.launch {
+            val result = gigyaRepository.logout()
+            if (result.isError()) {
+                error(result.error)
+                return@launch
+            }
+            onLogout()
+        }
+    }
+
+    fun socialLogin(provider: String, error: (GigyaError?) -> Unit, onLogin: () -> Unit) {
+        viewModelScope.launch {
+            val result = gigyaRepository.socialLoginWithProvider(provider)
+            if (result.isError()) {
+                error(result.error)
+                return@launch
+            }
+            onLogin()
+        }
+    }
+
+    fun passwordlessLogin(resultHandler: ActivityResultLauncher<IntentSenderRequest>,
+                          error: (GigyaError?) -> Unit, onLogin: () -> Unit) {
+        viewModelScope.launch {
+            val result = gigyaRepository.webAuthnLogin(resultHandler)
+            if (result.isError()) {
+                error(result.error)
+                return@launch
+            }
+            onLogin()
+        }
+    }
+
+    fun showScreenSets(screenset: String,
+                       error: (GigyaError?) -> Unit, onLogin: () -> Unit) {
+        gigyaRepository.gigyaInstance.showScreenSet(
+                screenset,
+                true,
+                mapOf(),
+                object : GigyaPluginCallback<MyAccount>() {
+
+                    override fun onLogin(accountObj: MyAccount) {
+                        account.value = accountObj
+                        onLogin()
+                    }
+
+                    override fun onError(event: GigyaPluginEvent?) {
+                        event?.let {
+                            val eventError = GigyaError.errorFrom(it.eventMap)
+                            error(eventError)
+                        }
+                    }
+
+                    // You can listen to additional events if required.
+                }
+        )
+    }
+}
