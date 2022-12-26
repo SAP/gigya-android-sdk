b6f274d2fc63975ec97f76f0dd01ff8a7d3a5947
Tal Mirmelshtein <tal.mirmelshtein@sap.com>
2022-12-26 11:58:11 +0200

    Updated NSS version 1.6.0

--- sdk-nss/src/main/java/com/gigya/android/sdk/nss/GigyaNss.java
+++ sdk-nss/src/main/java/com/gigya/android/sdk/nss/GigyaNss.java
@@ -27,7 +27,7 @@ import com.gigya.android.sdk.nss.engine.NssEngineLifeCycle;
 
 public class GigyaNss {
 
-    public static final String VERSION = "1.5.1";
+    public static final String VERSION = "1.6.0";
 
     private static final String LOG_TAG = "GigyaNss";
 
