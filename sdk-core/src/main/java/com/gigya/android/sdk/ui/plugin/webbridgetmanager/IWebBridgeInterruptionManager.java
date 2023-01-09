1ec6148a4a3674c6ed41025f245777053c342b5a
Tal Mirmelshtein <tal.mirmelshtein@sap.com>
2023-01-09 22:08:42 +0200

    Updated WebBridgeForceLoginResolver to support create account flow.
    Former-commit-id: 6f640a985e75f0506298357799a19705e7b0d420

--- sdk-core/src/main/java/com/gigya/android/sdk/ui/plugin/webbridgetmanager/IWebBridgeInterruptionManager.java
+++ sdk-core/src/main/java/com/gigya/android/sdk/ui/plugin/webbridgetmanager/IWebBridgeInterruptionManager.java
@@ -8,6 +8,9 @@ import java.util.Map;
 
 public interface IWebBridgeInterruptionManager<A extends GigyaAccount> {
 
+    void responseManager(
+            String api, Map<String, Object> params, A data, GigyaPluginFragment.IBridgeCallbacks<A> callback);
+
     void responseManager(Map<String, Object> params, A data, GigyaPluginFragment.IBridgeCallbacks<A> callback);
 
     void interruptionHandler(GigyaError error);
