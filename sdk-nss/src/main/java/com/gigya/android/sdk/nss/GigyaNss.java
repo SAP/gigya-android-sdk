17ff1f118bb3a1e708197ca64aafd8703c64d648
Tal Mirmelshtein <tal.mirmelshtein@sap.com>
2023-05-29 15:05:43 +0300

    Updating to NSS 1.7.1

--- sdk-nss/src/main/java/com/gigya/android/sdk/nss/GigyaNss.java
+++ sdk-nss/src/main/java/com/gigya/android/sdk/nss/GigyaNss.java
@@ -27,7 +27,7 @@ import com.gigya.android.sdk.nss.engine.NssEngineLifeCycle;
 
 public class GigyaNss {
 
-    public static final String VERSION = "1.7.0";
+    public static final String VERSION = "1.7.1";
 
     private static final String LOG_TAG = "GigyaNss";
 
