f2b8bb9aad8f3e3c1528e79f7822751a960fb7e9
Tal Mirmelshtein <tal.mirmelshtein@sap.com>
2024-01-10 11:12:22 +0200

    NSS version update 1.9.2

--- sdk-nss/src/main/java/com/gigya/android/sdk/nss/GigyaNss.java
+++ sdk-nss/src/main/java/com/gigya/android/sdk/nss/GigyaNss.java
@@ -29,7 +29,7 @@ import java.lang.reflect.InvocationTargetException;
 
 public class GigyaNss {
 
-    public static final String VERSION = "1.9.1";
+    public static final String VERSION = "1.9.2";
 
     private static final String LOG_TAG = "GigyaNss";
 
