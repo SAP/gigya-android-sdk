0b3ee50f24d1a2fff4a7f02a325fe225dfbd1905
Tal Mirmelshtein <tal.mirmelshtein@sap.com>
2025-09-28 23:30:07 +0300

    release: v7.2.1

--- sdk-core/src/main/java/com/gigya/android/sdk/Gigya.java
+++ sdk-core/src/main/java/com/gigya/android/sdk/Gigya.java
@@ -54,7 +54,7 @@ import java.util.TreeMap;
 public class Gigya<T extends GigyaAccount> {
 
     //region static
-    public static final String VERSION = "7.2.0";
+    public static final String VERSION = "7.2.1";
 
     private static final String LOG_TAG = "Gigya";
 
