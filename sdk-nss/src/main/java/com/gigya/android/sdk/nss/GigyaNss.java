176538474901e1f0ee1334a574789293eca2fe4c
Tal Mirmelshtein <tal.mirmelshtein@sap.com>
2025-12-29 11:15:28 +0200

    version: NSS wrapper version 1.9.11 (aligned with engine 1.9.9)

--- sdk-nss/src/main/java/com/gigya/android/sdk/nss/GigyaNss.java
+++ sdk-nss/src/main/java/com/gigya/android/sdk/nss/GigyaNss.java
@@ -29,7 +29,7 @@ import java.lang.reflect.InvocationTargetException;
 
 public class GigyaNss {
 
-    public static final String VERSION = "1.9.10";
+    public static final String VERSION = "1.9.11";
 
     private static final String LOG_TAG = "GigyaNss";
 
